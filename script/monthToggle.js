a3ae977638a4556c0b019efbf2c808663a10d347
shohei-shimizu <14683341+shohei-shimizu@users.noreply.github.com>
2024-10-23 05:48:05 +0900

    cssリファクタリング

--- script/monthToggle.js
+++ script/monthToggle.js
@@ -4,12 +4,22 @@ monthToggles.forEach(toggle => {
     toggle.addEventListener('click', function () {
         const postList = this.nextElementSibling;
 
-        if (!postList.classList.contains('hidden')) {
+        if (postList.classList.contains('show')) {
+            postList.classList.remove('show');
             postList.classList.add('hidden');
+            setTimeout(() => {
+                postList.style.display = 'none';
+            }, 200);
         } else {
-            document.querySelectorAll('.post-list').forEach(list => list.classList.add('hidden'));
+            document.querySelectorAll('.post-list').forEach(list => {
+                list.classList.remove('show');
+                list.classList.add('hidden');
+                postList.style.display = 'none';
+            });
 
+            postList.style.display = 'block';
             postList.classList.remove('hidden');
+            postList.classList.add('show');
         }
     });
 });
